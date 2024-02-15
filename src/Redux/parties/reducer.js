edf68969459d8a3c2f1cb84e69d4e3b11de99f60
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-02-16 02:27:09 +0530

    Harsh_test _1
    code

--- src/Redux/parties/reducer.js
+++ src/Redux/parties/reducer.js
@@ -1,29 +1,65 @@
-import { PARTIES_POST_FAILED, PARTIES_POST_REQUEST, PARTIES_POST_SUCCESS } from "./actionTypes"
+import {
+  PARTIES_POST_FAILED,
+  PARTIES_POST_REQUEST,
+  PARTIES_POST_SUCCESS,
+  PARTIES_PURCHASE_BILL_FAILURE,
+  PARTIES_PURCHASE_BILL_REQUEST,
+  PARTIES_PURCHASE_BILL_SUCCESS,
+  PARTIES_PAYMENT_OUT_FAILURE,
+  PARTIES_PAYMENT_OUT_REQUEST,
+  PARTIES_PAYMENT_OUT_SUCCESS,
+} from "./actionTypes";
 
+const initialState = {
+  partiesData: [],
+  isLoading: false,
+  isFailed: false,
+  purchaseBillData: [],
+  paymentOutData: [],
+};
 
-const initialState={
-    partiesData:[],
-    isLoading:false,
-    isFailied:false
-}
-
-export const reducer=(state=initialState,{type,payload})=>{
-switch(type){
+export const reducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case PARTIES_POST_REQUEST:
-        return {
-         ...state,
-            isLoading:true,
-            isFailied:false
-        }
-    case PARTIES_POST_SUCCESS: 
-       
-        return {
-             ...state,
-             isLoading:false,
-             partiesData: payload,
-        }
-        case PARTIES_POST_FAILED:
-          return {...state, isLoading:false ,isFailied:true}
-     default :return state;
-}
-}
+    case PARTIES_PURCHASE_BILL_REQUEST:
+    case PARTIES_PAYMENT_OUT_REQUEST:
+      return {
+        ...state,
+        isLoading: true,
+        isFailed: false,
+      };
+
+    case PARTIES_POST_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        partiesData: payload,
+      };
+
+    case PARTIES_PURCHASE_BILL_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        purchaseBillData: payload,
+      };
+
+    case PARTIES_PAYMENT_OUT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        paymentOutData: payload,
+      };
+
+    case PARTIES_POST_FAILED:
+    case PARTIES_PURCHASE_BILL_FAILURE:
+    case PARTIES_PAYMENT_OUT_FAILURE:
+      return {
+        ...state,
+        isLoading: false,
+        isFailed: true,
+      };
+
+    default:
+      return state;
+  }
+};
