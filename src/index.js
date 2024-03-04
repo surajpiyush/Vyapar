dc7f1a92c2a003b3e872ac9d519fc7e1f3f6855d
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2024-03-04 19:56:29 +0530

    added profile update

--- src/index.js
+++ src/index.js
@@ -43,142 +43,139 @@ import Marketing from "./pages/Marketing.jsx";
 import Setting from "./pages/setting/Setting.js";
 
 const router = createBrowserRouter([
-
-   {
-      path: "/",
-      element: <App />,
-      children: [
-         {
-            path: "/",
-            element: <Homepage />,
-         },
-         {
-            path: "/parties",
-            element: <Parties />,
-         },
-         {
-            path: "/items",
-            element: <Items />,
-         },
-         {
-            path: "/auth",
-            element: <Authpage />,
-         },
-         {
-            path: "/company",
-            element: <Companypage />,
-         },
-         {
-            path: "/purchasebill",
-            element: <Purchasebill />,
-         },
-         {
-            path: "/paymentout",
-            element: <Paymentout />,
-         },
-         {
-            path: "/paymentorder",
-            element: <Purchaseorderpage />,
-         },
-         {
-            path: "/purchasereturn",
-            element: <Purchasereturnpage />,
-         },
-         {
-            path: "/addpurchase",
-            element: <AddPurchase />,
-         },
-         {
-            path: "/addpurchaseorder",
-            element: <Addpurchaseorderpage />,
-         },
-         {
-            path: "/addpurchasereturn",
-            element: <AddPurchasereturn />,
-         },
-         {
-            path: "/business",
-            element: <Busniess />,
-         },
-         // Sales Routes
-         {
-            path: "/invoices",
-            element: <SalesInvoice />,
-         },
-         {
-            path: "/estimates",
-            element: <SalesEstimates />,
-         },
-         {
-            path: "/paymentin",
-            element: <SalesPaymentln />,
-         },
-         {
-            path: "/saleorder",
-            element: <SalesOrder />,
-         },
-         {
-            path: "/deliverychallan",
-            element: <SalesDeliveryChallan />,
-         },
-         {
-            path: "/salereturn",
-            element: <SalesReturn />,
-         },
-         {
-            path: "/salereport",
-            element: <Sale />,
-         },
-         {
-            path: "/purchasereport",
-            element: <Purchase />,
-         },
-         {
-            path: "/daybookreport",
-            element: <DayBook />,
-         },
-         {
-            path: "/alltransactionreport",
-            element: <AllTransaction />,
-         },
-         {
-            path: "/cashflowreport",
-            element: <CashFlow />,
-         },
-         {
-            path: "/gstr1report",
-            element: <GSTR1 />,
-         },
-         {
-            path: "/gstr2report",
-            element: <GSTR2 />,
-         },
-         {
-            path: "/gstr3breport",
-            element: <GSTR3B />,
-         },
-         {
-            path: "/gstr9report",
-            element: <GSTR9 />,
-         },
-         {
-            path: "/salehsnreport",
-            element: <SaleHSN />,
-         },
-         {
-            path: "/formpage",
-            element: <Formpage />,
-         },
-         { path: "/marketing", element: <Marketing /> },
-         { path: "/setting", element: <Setting /> },
-      ],
-   },
-
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/parties",
+        element: <Parties />,
+      },
+      {
+        path: "/items",
+        element: <Items />,
+      },
+      {
+        path: "/auth",
+        element: <AuthPage />,
+      },
+      {
+        path: "/company",
+        element: <Companypage />,
+      },
+      {
+        path: "/purchasebill",
+        element: <Purchasebill />,
+      },
+      {
+        path: "/paymentout",
+        element: <Paymentout />,
+      },
+      {
+        path: "/paymentorder",
+        element: <Purchaseorderpage />,
+      },
+      {
+        path: "/purchasereturn",
+        element: <Purchasereturnpage />,
+      },
+      {
+        path: "/addpurchase",
+        element: <AddPurchase />,
+      },
+      {
+        path: "/addpurchaseorder",
+        element: <Addpurchaseorderpage />,
+      },
+      {
+        path: "/addpurchasereturn",
+        element: <AddPurchasereturn />,
+      },
+      {
+        path: "/business",
+        element: <Busniess />,
+      },
+      // Sales Routes
+      {
+        path: "/invoices",
+        element: <SalesInvoice />,
+      },
+      {
+        path: "/estimates",
+        element: <SalesEstimates />,
+      },
+      {
+        path: "/paymentin",
+        element: <SalesPaymentln />,
+      },
+      {
+        path: "/saleorder",
+        element: <SalesOrder />,
+      },
+      {
+        path: "/deliverychallan",
+        element: <SalesDeliveryChallan />,
+      },
+      {
+        path: "/salereturn",
+        element: <SalesReturn />,
+      },
+      {
+        path: "/salereport",
+        element: <Sale />,
+      },
+      {
+        path: "/purchasereport",
+        element: <Purchase />,
+      },
+      {
+        path: "/daybookreport",
+        element: <DayBook />,
+      },
+      {
+        path: "/alltransactionreport",
+        element: <AllTransaction />,
+      },
+      {
+        path: "/cashflowreport",
+        element: <CashFlow />,
+      },
+      {
+        path: "/gstr1report",
+        element: <GSTR1 />,
+      },
+      {
+        path: "/gstr2report",
+        element: <GSTR2 />,
+      },
+      {
+        path: "/gstr3breport",
+        element: <GSTR3B />,
+      },
+      {
+        path: "/gstr9report",
+        element: <GSTR9 />,
+      },
+      {
+        path: "/salehsnreport",
+        element: <HsnReport />,
+      },
+      {
+        path: "/formpage",
+        element: <Formpage />,
+      },
+      { path: "/marketing", element: <Marketing /> },
+      { path: "/setting", element: <Setting /> },
+    ],
+  },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-
   <Provider store={store}>
     <ChakraProvider>
       <RouterProvider router={router} />
