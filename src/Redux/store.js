f152bde4b0ccae5043b40ff64756c09104228467
AK050688 <90385626+AK050688@users.noreply.github.com>
2024-03-20 23:22:19 +0530

    Update store.js

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,7 @@
 import { applyMiddleware, combineReducers, compose, createStore } from "redux";
 import { thunk } from "redux-thunk";
 
-import { reducer as PartiesReducer } from "./Parties/reducer";
+import { reducer as PartiesReducer } from "./parties/reducer";
 import { reducer as BusinessReducer } from "./business/reducer";
 import { reducer as ItemReducer } from "./items/reducer";
 import { reducer as PurchaseReducer } from "./purchase/reducer";
