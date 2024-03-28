be66b267bda24072a46365792ae5af3d82a620f3
Hariom70890 <hariomsunilverma@gmail.com>
2024-03-28 11:21:57 +0530

    index update

--- src/Page/parties/Parties.jsx
+++ src/Page/parties/Parties.jsx
@@ -1,11 +1,11 @@
-import css from "./Parties.module.css";
-import AddPartyForm from "./AddPartyForm";
-import party from "../../assets/Images/party.jpg";
+import FirstTimeFormToggle from "../../Component/FirmTimeForm/FirstTimeFormToggle";
 import Loader1 from "../../Component/Loaders/Loader1";
 import Setting from "../../Component/Setting/Setting";
-import PartiesTable from "../../components/TableData/PartiesTable";
-import FirstTimeFormToggle from "../../Component/FirmTimeForm/FirstTimeFormToggle";
 import { FetchAllParties } from "../../Redux/parties/actions";
+import party from "../../assets/Images/party.jpg";
+import PartiesTable from "../../components/TableData/PartiesTable";
+import AddPartyForm from "./AddPartyForm";
+import css from "./Parties.module.css";
 
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -15,12 +15,13 @@ export default function Parties() {
   const isLoading = useSelector((state) => state.PartiesReducer.isLoading);
   const togglePartiesData = useSelector(
     (state) => state.PartiesReducer.togglePartiesData
-  );
+    );
   const partiesData = useSelector((state) => state.PartiesReducer.partiesData);
-
+  
   const [toggleSetting, setToggleSetting] = useState(false);
   const [partyFormIsOpen, setPartyFormIsOpen] = useState(false);
-
+  
+  console.log("first")
   // Fetch All Parties
   useEffect(() => {
     FetchAllParties(dispatch);
@@ -29,6 +30,8 @@ export default function Parties() {
   const dataFromChild = (val) => {
     setPartyFormIsOpen(val);
   };
+
+  // Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia iste reiciendis nisi perferendis similique unde eius atque nostrum ipsa. Illum unde accusamus inventore esse aliquam temporibus, iusto, eligendi vero perspiciatis architecto, labore obcaecati eveniet expedita! Possimus similique, enim voluptatibus eaque velit vero. Officiis veritatis placeat recusandae illo, minus aut dolorem error tempora officia sapiente quis vel. Accusantium qui quas excepturi architecto atque rerum dignissimos. Accusantium distinctio quae tenetur dolorem unde veritatis ipsum, nesciunt sunt autem, quidem necessitatibus eveniet ratione, non iure nobis fuga numquam ad repellendus.
 
   return (
     <div>
