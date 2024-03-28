6f94e02438df8dff14ef6de5f7cc1299962b2e6a
Hariom70890 <hariomsunilverma@gmail.com>
2024-03-28 19:24:43 +0530

    puchase done

--- src/Page/Purchase/Purchasereturnpage.jsx
+++ src/Page/Purchase/Purchasereturnpage.jsx
@@ -18,76 +18,87 @@ import { useDispatch, useSelector } from "react-redux";
 import Addpurchasereturnitrm from "../../Component/Purchase/Addpurchasereturnitrm";
 import { useLocation, useNavigate } from "react-router-dom";
 import Setting from "../../Component/Setting/Setting";
+import { getPurchaseReturn } from "../../Redux/purchase/action";
 const Purchasereturnpage = () => {
-  const [toggleSetting, setToggleSetting] = useState(false);
-  const toast = useToast();
-  const navigate = useNavigate();
-  const location = useLocation();
-  const dispatch = useDispatch();
-  const [openForm, setOpenForm] = useState(false);
-  const [startDate, setStartDate] = useState("2024-02-01");
-  const [endDate, setEndDate] = useState(
-    new Date().toISOString().split("T")[0]
-  );
-  const toggleSalesSuccess = useSelector(
-    (state) => state.SalesReducer.toggleSalesSuccess
-  );
-  const isLoading = useSelector((state) => state.SalesReducer.isLoading);
+   const [toggleSetting, setToggleSetting] = useState(false);
+   const toast = useToast();
+   const navigate = useNavigate();
+   const location = useLocation();
+   const dispatch = useDispatch();
+   const [openForm, setOpenForm] = useState(false);
+   const [startDate, setStartDate] = useState("2024-02-01");
+   const [endDate, setEndDate] = useState(
+      new Date().toISOString().split("T")[0]
+   );
 
-  useEffect(() => {
-    GetAllSalesInvoice(dispatch, startDate, endDate);
-  }, [toggleSalesSuccess, startDate, endDate]);
+   const isLoading = useSelector((state) => state.SalesReducer.isLoading);
+   const data = useSelector(
+      (store) => store.PurchaseReducer?.purchaseReturnData
+   );
+   const date = { startDate: startDate, endDate: endDate };
 
-  const formOpen = () => {
-    setOpenForm(true);
-  };
-  const date = { startDate: startDate, endDate: endDate };
+   useEffect(() => {
+      dispatch(getPurchaseReturn({ date }));
+   }, [dispatch, startDate, endDate]);
 
-  return (
-    <div className="purchase-bill-container">
-      <Thismonth
-        startDate={startDate}
-        endDate={endDate}
-        setStartDate={setStartDate}
-        setEndDate={setEndDate}
-      />
-      <div>
-        {toggleSetting && <Setting setToggleSetting={setToggleSetting} />}
-        <div>
-          {openForm ? (
-            <div className={css.formOuter}>
-              <div className={css.upperNav}>
-                <div>
-                  <p className={css.activeForm}>
-                    <span>Sale #1</span>
-                    <CrossIcon />
-                  </p>
-                </div>
-                <div>
-                  <CalculatorIcon
-                    onClick={() =>
-                      toast({
-                        title: "Feature currently in development",
-                        status: "info",
-                        position: "top",
-                      })
-                    }
-                  />
-                  <SettingIcon onClick={() => setToggleSetting(true)} />
-                  <CloseIcon onClick={() => setOpenForm(false)} />
-                </div>
-              </div>
-              <Addpurchasereturnitrm setOpenForm={setOpenForm} />
-            </div>
-          ) : (
+   const formOpen = () => {
+      setOpenForm(true);
+   };
+
+   return (
+      <div className="purchase-bill-container">
+         <Thismonth
+            startDate={startDate}
+            endDate={endDate}
+            setStartDate={setStartDate}
+            setEndDate={setEndDate}
+            data={data}
+         />
+         <div>
+            {toggleSetting && <Setting setToggleSetting={setToggleSetting} />}
             <div>
-              {!isLoading && <Purchasereturn func={formOpen} date={date} />}
+               {openForm ? (
+                  <div className={css.formOuter}>
+                     <div className={css.upperNav}>
+                        <div>
+                           <p className={css.activeForm}>
+                              <span>Sale #1</span>
+                              <CrossIcon />
+                           </p>
+                        </div>
+                        <div>
+                           <CalculatorIcon
+                              onClick={() =>
+                                 toast({
+                                    title: "Feature currently in development",
+                                    status: "info",
+                                    position: "top",
+                                 })
+                              }
+                           />
+                           <SettingIcon
+                              onClick={() => setToggleSetting(true)}
+                           />
+                           <CloseIcon onClick={() => setOpenForm(false)} />
+                        </div>
+                     </div>
+                     <Addpurchasereturnitrm setOpenForm={setOpenForm} />
+                  </div>
+               ) : (
+                  <div>
+                     {!isLoading && (
+                        <Purchasereturn
+                           func={formOpen}
+                           date={date}
+                           data={data}
+                        />
+                     )}
+                  </div>
+               )}
             </div>
-          )}
-        </div>
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default Purchasereturnpage;
