3b75e67d786777d3855bae918e70db0657d1b1e6
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2024-03-30 22:02:15 +0530

    updated

--- src/assets/Icons/ReactIcons.jsx
+++ src/assets/Icons/ReactIcons.jsx
@@ -4,31 +4,79 @@ import { FaPlus as PlusIconThin } from "react-icons/fa6";
 import { RiDeleteBinLine as DeleteIcon } from "react-icons/ri";
 import { ImSpinner3 as BasicSpinnerIcon } from "react-icons/im";
 import { FiMinusCircle as MinusCircleIcon } from "react-icons/fi";
-import { BsFillPlusCircleFill as PlusIcon2 } from "react-icons/bs";
-import { FaPlus as PlusIcon, FaSearch as SearchIcon2 } from "react-icons/fa";
+import { AiFillTool as UtilitesIcon } from "react-icons/ai";
+import {
+  BsBarChartFill as StatsIcon,
+  BsFillPlusCircleFill as PlusIcon2,
+} from "react-icons/bs";
+import {
+  TbReport as DocumentIcon,
+  TbTriangleSquareCircleFilled as ShapesIconFilled,
+  TbDeviceRemote as RemoteDeviceIcon,
+} from "react-icons/tb";
+import {
+  BiSolidBank as BankIcon,
+  BiUpArrowAlt as UpArrowIcon2,
+} from "react-icons/bi";
+import {
+  FaPlus as PlusIcon,
+  FaSearch as SearchIcon2,
+  FaRupeeSign as RupeesIcon,
+  FaCaretUp as ArrowUpFilledIcon,
+  FaCaretDown as ArrowDownFilledIcon,
+  FaWhatsapp as WhatsAppOutlineIcon,
+} from "react-icons/fa";
 import {
   PiToggleLeftFill as OpenToggleIcon,
   PiToggleRightThin as CloseToggleIcon,
   PiDotsThreeVerticalBold as VerticalDotsIcon,
 } from "react-icons/pi";
 import {
+  IoWallet as WalletIcon,
   IoSettingsSharp as SettingsIconFilled,
   IoSettingsOutline as SettingsIconOutline,
   IoSearchCircleSharp as SearchIconBlackBg,
+  IoStorefront as ShopIconFilled,
+  IoCart as CartIconFilled,
 } from "react-icons/io5";
 import {
   IoMdClose as CloseIcon,
   IoIosSearch as SearchIcon,
+  IoIosArrowDown as DownArrowIcon,
+  IoMdHome as HomeIcon,
   IoMdRefresh as RefreshIcon,
   IoMdCloseCircle as CloseIcon2,
   IoIosArrowRoundUp as UpArrowIcon,
 } from "react-icons/io";
+import {
+  MdOutlineSettingsBackupRestore as BackupRestoreIcon,
+  MdPeopleAlt as PeopleIconFilled,
+  MdOutlineArrowForwardIos as RightArrowIcon,
+} from "react-icons/md";
 
 // ------------------------------- EXPORTS ---------------------------------
 export {
+  StatsIcon,
+  ShapesIconFilled,
+  RemoteDeviceIcon,
+  WhatsAppOutlineIcon,
+  ShopIconFilled,
+  CartIconFilled,
+  BackupRestoreIcon,
+  PeopleIconFilled,
+  RightArrowIcon,
+  BankIcon,
   PlusIcon,
+  UtilitesIcon,
   PlusIcon2,
+  DownArrowIcon,
+  ArrowUpFilledIcon,
+  UpArrowIcon2,
+  ArrowDownFilledIcon,
+  RupeesIcon,
+  HomeIcon,
   CrossIcon,
+  WalletIcon,
   CloseIcon,
   CloseIcon2,
   FilterIcon,
@@ -38,6 +86,7 @@ export {
   SearchIcon2,
   UpArrowIcon,
   PlusIconThin,
+  DocumentIcon,
   OpenToggleIcon,
   CloseToggleIcon,
   MinusCircleIcon,
