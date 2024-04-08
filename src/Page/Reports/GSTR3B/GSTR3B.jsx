0e39f98dbdfc172849c795f96d7c9fd31b16d7a4
prateekshuklaps0 <prateekshuklaps0@gmail.com>
2024-04-08 11:58:07 +0530

    update

---
+++ src/Page/Reports/GSTR3B/GSTR3B.jsx
@@ -0,0 +1,11 @@
+import css from "./GSTR3B.module.css";
+import ReportUpperControlPanel from "../../../Component/UpperControlPanel/ReportUpperControlPanel";
+
+import { useState } from "react";
+import { useSelector } from "react-redux";
+
+const GSTR3B = () => {
+  return <div>GSTR3B</div>;
+};
+
+export default GSTR3B;
